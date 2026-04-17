4e969cf7a678ba13afdc23912ad870897d392c19
Being-RAYtastic <risheet.ray@gmail.com>
2026-04-17 17:47:45 +0530

    added breakTime

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const timerDisplay = document.getElementById('timer')
 const actionTimerButton = document.getElementById('actionTimerBtn')
 const resetButton = document.getElementById('resetTimerBtn')
 const settingsButton = document.getElementById('settingsBtn')
-const saveBtn = document.getElementById('saveBtn')
+const saveFocusBtn = document.getElementById('saveFocusBtn')
+
 
 const timeOverAudio = document.getElementById("timeOver")
 timeOverAudio.volume = 0
 
 
 let timerCount = 0
+
+let breakTimerCount = 0
 let timerIsRunning = false
+
+let mode = "focus"
+
 const defaultTimerCount = 1500
 
 if (timerCount === 0) {
@@ -26,9 +32,9 @@ function formatTime(totalSeconds) {
 
     if (hours > 0) {
         return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
-    
+
     } else {
-            
+
         return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
     }
 }
@@ -40,12 +46,12 @@ function startTimer() {
     if (timerIsRunning) settingsButton.classList.add('hidden')
 
     const timer = setInterval(() => {
-        
+
         if (timerCount > 0 && timerIsRunning) {
             timerCount--
             // console.log(timerCount)
             timerDisplay.textContent = formatTime(timerCount)
-        } 
+        }
         else {
             timerIsRunning = false
             clearInterval(timer)
@@ -57,8 +63,8 @@ function startTimer() {
 
             if (timerCount === 0) {
                 timeOverAudio.play()
-                
-                loadFocusTime()
+
+                switchMode()
                 settingsButton.classList.remove('hidden')
             }
         }
@@ -76,18 +82,18 @@ function pauseTimer() {
 }
 
 // START/PAUSE button event listener
-actionTimerButton.addEventListener('click', ()=> {
+actionTimerButton.addEventListener('click', () => {
     if (!timerIsRunning) {
         startTimer()
         actionTimerButton.classList.remove('fa-play')
         actionTimerButton.classList.add('fa-pause')
-        
+
     } else {
         pauseTimer()
         actionTimerButton.classList.remove('fa-pause')
         actionTimerButton.classList.add('fa-play')
     }
-    
+
 })
 
 // Reset button event listener
@@ -97,7 +103,7 @@ resetButton.addEventListener('click', () => {
     actionTimerButton.classList.remove('fa-pause')
     actionTimerButton.classList.add('fa-play')
     loadFocusTime()
-}) 
+})
 
 // Settings button event listener
 settingsButton.addEventListener('click', () => {
@@ -106,20 +112,41 @@ settingsButton.addEventListener('click', () => {
 })
 
 // Save button event listener (to save focus time settings)
-saveBtn.addEventListener('click', () => {
-    const hoursInput = document.getElementById('hours').value || 0
-    const minutesInput = document.getElementById('minutes').value || 0
-    const secondsInput = document.getElementById('seconds').value || 0
+saveFocusBtn.addEventListener('click', () => {
+    const focusHoursInput = document.getElementById('focusHours').value || 0
+    const focusMinutesInput = document.getElementById('focusMinutes').value || 0
+    const focusSecondsInput = document.getElementById('focusSeconds').value || 0
 
 
-    const totalSeconds = (parseInt(hoursInput) * 3600) + (parseInt(minutesInput) * 60) + parseInt(secondsInput)
+    const totalSeconds = (parseInt(focusHoursInput) * 3600) + (parseInt(focusMinutesInput) * 60) + parseInt(focusSecondsInput)
 
     if (totalSeconds > 0) localStorage.setItem("focusTime", JSON.stringify(totalSeconds));
     else if (totalSeconds === 0) localStorage.setItem("focusTime", JSON.stringify(defaultTimerCount));
-    
+
     loadFocusTime()
 
 })
+
+saveBreakBtn.addEventListener('click', () => {
+    const breakHoursInput = document.getElementById('breakHours').value || 0
+    const breakMinutesInput = document.getElementById('breakMinutes').value || 0
+    const breakSecondsInput = document.getElementById('breakSeconds').value || 0
+
+    const totalSeconds = (parseInt(breakHoursInput) * 3600) + (parseInt(breakMinutesInput) * 60) + parseInt(breakSecondsInput)
+
+    if (totalSeconds > 0) localStorage.setItem("breakTime", JSON.stringify(totalSeconds));
+    else if (totalSeconds === 0) localStorage.setItem("breakTime", JSON.stringify(defaultTimerCount));
+
+    // loadBreakTime()
+})
+
+function loadBreakTime() {
+    const storedBreakTime = localStorage.getItem("breakTime");
+    if (storedBreakTime) {
+        timerCount = JSON.parse(storedBreakTime);
+        timerDisplay.textContent = formatTime(timerCount);
+    }
+}
 
 // load focus time from localStorage on calling the function
 function loadFocusTime() {
@@ -129,3 +156,45 @@ function loadFocusTime() {
         timerDisplay.textContent = formatTime(timerCount);
     }
 }
+
+function switchMode(manualSelection) {
+
+
+    if (manualSelection === undefined) {
+
+        if (mode === "focus") {
+            mode = "break"
+            loadBreakTime()
+        } else if (mode === "break") {
+            mode = "focus"
+            loadFocusTime()
+        }
+    }
+    // else if (manualSelection === "focus") {
+    //     mode = "focus"
+    //     loadFocusTime()
+    // } else if (manualSelection === "break") {
+    //     mode = "break"
+    //     loadBreakTime()
+    // }
+}
+
+
+const focusSettingsMenuBtn = document.getElementById('focusSettingsMenuBtn')
+const breakSettingsMenuBtn = document.getElementById('breakSettingsMenuBtn')
+
+focusSettingsMenuBtn.addEventListener('click', () => {
+    focusSettingsMenuBtn.classList.add('active')
+    breakSettingsMenuBtn.classList.remove('active')
+
+    document.querySelector('.focusSettings').classList.remove('hidden')
+    document.querySelector('.breakSettings').classList.add('hidden')
+})
+
+breakSettingsMenuBtn.addEventListener('click', () => {
+    breakSettingsMenuBtn.classList.add('active')
+    focusSettingsMenuBtn.classList.remove('active')
+
+    document.querySelector('.breakSettings').classList.remove('hidden')
+    document.querySelector('.focusSettings').classList.add('hidden')
+})
